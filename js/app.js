8f70535bd07c52577ddeb44a827677b8ffaad3d9
Anderson Ramalho <brunnowtf@gmail.com>
2021-11-19 09:53:41 -0300

    Font-family changed

--- js/app.js
+++ js/app.js
@@ -31,7 +31,5 @@ const accordion = document.getElementsByClassName('contentBx');
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener('click', function(){
     this.classList.toggle('active-accordion');
-
-    
   })
 }
